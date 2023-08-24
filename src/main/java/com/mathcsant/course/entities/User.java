be010b4633944782975a2bb6d3f33698158bcd55
Matheus Campanhola <matheuscampanholasantos123@gmail.com>
2023-08-23 23:10:15 -0300

    Ajuste nas rotas dos produtos

--- src/main/java/com/mathcsant/course/entities/User.java
+++ src/main/java/com/mathcsant/course/entities/User.java
@@ -26,13 +26,15 @@ public class User implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotBlank(message = "Name not found/invalid")
 	private String name;
 	@Email
 	@NotBlank(message = "Email not found/invalid")
 	private String email;
 	@NotBlank(message = "Phone not found/invalid")
+	// Proposta melhoria, criar um DTO e utilizar o @JsonSerialize para formata o
+	// telefone em (XX) XXXXX-XXXX
 	private String phone;
 	@NotBlank(message = "Password not found/invalid")
 	private String password;
--- src/main/java/com/mathcsant/course/services/ProductService.java
+++ src/main/java/com/mathcsant/course/services/ProductService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.mathcsant.course.entities.Product;
+import com.mathcsant.course.repositories.CategoryRepository;
 import com.mathcsant.course.repositories.ProductRepository;
 import com.mathcsant.course.services.exceptions.DataBaseException;
 import com.mathcsant.course.services.exceptions.ResourceNotFoundException;
@@ -18,46 +19,60 @@ import com.mathcsant.course.utils.entities.updateEntities;
 public class ProductService {
 
 	@Autowired
-	private ProductRepository repository;
+	private ProductRepository productRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public List<Product> findAll() {
-		return repository.findAll();
+		return productRepository.findAll();
 	}
 
 	public Product findById(Long id) {
-		Optional<Product> p = repository.findById(id);
+		Optional<Product> p = productRepository.findById(id);
 		return p.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public Product createProduct(Product product) {
-		return repository.save(product);
+		validateCategory(product);
+		return productRepository.save(product);
 	}
 
 	public List<Product> createProducts(List<Product> products) {
+		products.forEach(p -> validateCategory(p));
 		List<Product> createdProducts = new ArrayList<>();
-		products.forEach(p -> createdProducts.add(repository.save(p)));
+		products.forEach(p -> createdProducts.add(productRepository.save(p)));
 		return createdProducts;
 	}
 
 	public Product updateProduct(Long id, Product updatedProduct) {
-		if (!repository.existsById(id)) {
+		if (!productRepository.existsById(id)) {
 			throw new ResourceNotFoundException(id);
 		}
-		Product existingProduct = repository.getReferenceById(id);
-
+		validateCategory(updatedProduct);
+		Product existingProduct = productRepository.getReferenceById(id);
 		updateEntities.updateProduct(updatedProduct, existingProduct);
-
-		return repository.save(existingProduct);
+		return productRepository.save(existingProduct);
 	}
 
 	public void deleteProductById(Long id) {
-		if (!repository.existsById(id)) {
+		if (!productRepository.existsById(id)) {
 			throw new ResourceNotFoundException(id);
 		}
 		try {
-			repository.deleteById(id);
+			productRepository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DataBaseException(e.getMessage());
+		}
+	}
+
+	private void validateCategory(Product product) {
+		if (!product.getCategories().isEmpty()) {
+			product.getCategories().forEach(c -> {
+				if (!categoryRepository.existsById(c.getId()) || 
+						!categoryRepository.getReferenceById(c.getId()).getName().equalsIgnoreCase(c.getName())) {
+					throw new ResourceNotFoundException(c.getId());
+				}
+			});
 		}
 	}
 
